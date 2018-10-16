dca5d48d8d30a4df274ddde525f85f3762c8830b
shaozhengbo <shaoISzhengbo@163.com>
2018-10-17 00:37:18 +0800

    首页设计

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,17 +1,25 @@
-var vm = new Vue({
-	el : '#app',
-	data : {
-		username : "1",
-		password : "1",
-		result : "请登录"
-	},
-	methods : {
-		login() {
-			axios({
-		        method:'get',
-		         url:'User/getUserByUsername?username='+vm.username
-		     }).then(function(res){
-		     });
-		}
-	}
-});
+//var vm = new Vue({
+//	el : '#app',
+//	data : {
+//		username : "1",
+//		password : "1",
+//		result : "请登录"
+//	},
+//	methods : {
+//		login() {
+//			axios({
+//		        method:'get',
+//		         url:'User/getUserByUsername?username='+vm.username
+//		     }).then(function(res){
+//		     });
+//		}
+//	}
+//});
+
+function changeColor(a) {
+	a.style.color = '#eb7350';
+}
+
+function returnColor(a) {
+	a.style.color = '';
+}
