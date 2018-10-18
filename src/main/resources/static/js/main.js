c32720dc7aecbad21801cd1efa284151c919bee7
shaozhengbo <shaoISzhengbo@163.com>
2018-10-18 20:07:04 +0800

    jQuery实现动态标签变色

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,23 +3,58 @@
 //	data : {
 //		username : "1",
 //		password : "1",
-//		result : "请登录"
+//		result : "请登录",
+//		a : false
 //	},
 //	methods : {
-//		login() {
-//			axios({
-//		        method:'get',
-//		         url:'User/getUserByUsername?username='+vm.username
-//		     }).then(function(res){
-//		     });
+////		login() {
+////			axios({
+////		        method:'get',
+////		         url:'User/getUserByUsername?username='+vm.username
+////		     }).then(function(res){
+////		     });
+////		},
+//		letFontBeOrange : function() {
+//			a = true;
+//		},
+//		returnColor : function() {
+//			a = false;
 //		}
+//		
 //	}
 //});
+$(document).ready(function(){
+ 
+	$('#a').mouseenter(function() {
+		$('#a').css("color","#eb7350");
+	});
+	
+	$('#a').mouseleave(function() {
+		$('#a').css("color","");
+	});
+	
+	$('#b').mouseenter(function() {
+		$('#b').css("color","#eb7350");
+	});
+	
+	$('#b').mouseleave(function() {
+		$('#b').css("color","");
+	});
+	
+	$('#c').mouseenter(function() {
+		$('#c').css("color","#eb7350");
+	});
+	
+	$('#c').mouseleave(function() {
+		$('#c').css("color","");
+	});
+	
+	$('#d').mouseenter(function() {
+		$('#d').css("color","#eb7350");
+	});
+	
+	$('#d').mouseleave(function() {
+		$('#d').css("color","");
+	});
+});
 
-function changeColor(a) {
-	a.style.color = '#eb7350';
-}
-
-function returnColor(a) {
-	a.style.color = '';
-}
