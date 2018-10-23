4c91c75ff10c1acddf62a3219f510f4d337e1d77
shaozhengbo <shaoISzhengbo@163.com>
2018-10-23 21:46:45 +0800

    登陆有问题

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,58 +23,81 @@
 //		
 //	}
 //});
-$(document).ready(function(){
- 
+$(document).ready(function() {
+
 	$('#a').mouseenter(function() {
-		$('#a').css("color","#eb7350");
+		$('#a').css("color", "#eb7350");
 	});
-	
+
 	$('#a').mouseleave(function() {
-		$('#a').css("color","");
+		$('#a').css("color", "");
 	});
-	
+
 	$('#b').mouseenter(function() {
-		$('#b').css("color","#eb7350");
+		$('#b').css("color", "#eb7350");
 	});
-	
+
 	$('#b').mouseleave(function() {
-		$('#b').css("color","");
+		$('#b').css("color", "");
 	});
-	
+
 	$('#c').mouseenter(function() {
-		$('#c').css("color","#eb7350");
+		$('#c').css("color", "#eb7350");
 	});
-	
+
 	$('#c').mouseleave(function() {
-		$('#c').css("color","");
+		$('#c').css("color", "");
 	});
-	
+
 	$('#d').mouseenter(function() {
-		$('#d').css("color","#eb7350");
+		$('#d').css("color", "#eb7350");
 	});
-	
+
 	$('#d').mouseleave(function() {
-		$('#d').css("color","");
+		$('#d').css("color", "");
 	});
-	
 	$('#tab-login').addClass("tab-active");
+	$('#tab-register').addClass("tab-unactive");
 });
 
 function clickTab(id) {
 	var login = $('#tab-login');
 	var register = $('#tab-register');
-	if(id=='tab-login') {
-		if(!login.hasClass("tab-active")) {
+	if (id == 'tab-login') {
+		if (!login.hasClass("tab-active")) {
+			login.removeClass("tab-unactive");
 			login.addClass("tab-active");
 			register.removeClass("tab-active");
 			register.addClass("tab-unactive");
 		}
-	} else if(id=="tab-register") {
-		if(!register.hasClass("tab-active")) {
+	} else if (id == "tab-register") {
+		if (!register.hasClass("tab-active")) {
+			register.removeClass("tab-unactive");
 			register.addClass("tab-active");
 			login.removeClass("tab-active");
 			login.addClass("tab-unactive");
 		}
 	}
-}
+};
 
+function login() {
+	var username = $('#inputUsername').val();
+	var password = $('#inputPassword').val();
+	$.ajax({
+		url : "/User/getUserByUsernameAndPassword",
+		type : "post",
+		data : {
+			"username" : username,
+			"password" : password
+		},
+		success : function(data) {
+			$('#loginDiv').fadeOut("fast");
+			$('#a').html(data.object.username);
+			$('#b').html("退出");
+			alert(data.msg);
+		},
+		error : function(data) {
+			console.log(data);
+		}
+	});
+}
